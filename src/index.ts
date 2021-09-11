54e2e55bde18fdb419c2ae288967558a84ad39e8
MrBartusek <bartusekcraft@gmail.com>
2021-09-11 18:40:15 +0200

    Project structure and minimal functionality

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import * as simpleIcons from 'simple-icons';
+import * as urls from '../data/urls.json';
+
+export const enum ExportType {
+	URL = 'URL',
+	HTML = 'HTML',
+	MARKDOWN = 'MARKDOWN'
+}
+
+export function get(name: string, type?: ExportType | 'URL' | 'HTML' | 'MARKDOWN', includeURL?: boolean): string {
+	if(type == undefined) type = ExportType.URL;
+	if(includeURL == undefined) includeURL = true;
+
+	const icon = simpleIcons.get(name);
+	const shieldURL = `https://img.shields.io/badge/-${icon.title}-${icon.hex}?style=flat-square&logo=${icon.slug}&logoColor=white`;
+	const url = serviceURL(icon);
+
+	if(type == ExportType.URL) {
+		return shieldURL;
+	}
+	else if(type == ExportType.HTML) {
+		const htmlImage = `<img alt="${icon.title}" src="${shieldURL}"/>`;
+		return includeURL ? `<a href="${url}">${htmlImage}</a>` : htmlImage;
+	}
+	else if(type == ExportType.MARKDOWN) {
+		const markdownImage = `![${icon.title}](${shieldURL})`;
+		return includeURL ? `[${markdownImage}](${url})` : markdownImage;
+	}
+	else {
+		throw new Error('Invalid argument for type');
+	}
+}
+
+function serviceURL(icon: simpleIcons.SimpleIcon): string {
+	if(Object.prototype.hasOwnProperty.call(urls.urls, icon.slug)) {
+		return (urls.urls as any)[icon.slug];
+	}
+	else {
+		return baseURL(icon.source);
+	}
+}
+
+function baseURL(url: string): string {
+	const path = url.split( '/' );
+	return path[0] + '//' + path[2] + '/';
+}
